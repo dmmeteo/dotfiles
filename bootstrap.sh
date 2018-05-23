d716763cbe8ac9b72c93622f7a3c3ab63d9c9f43
Dmitry Sichkar <dmmeteo@gmail.com>
2018-05-23 13:42:40 +0300

    vim

---
+++ bootstrap.sh
@@ -0,0 +1,34 @@
+#!/bin/sh
+DOTFILES=$HOME/dotfiles
+
+# Delete existing dot files and folders
+sudo rm -rf ~/.zshrc > /dev/null 2>&1
+sudo rm -rf ~/.gitconfig > /dev/null 2>&1
+sudo rm -rf ~/.gitignore_global > /dev/null 2>&1
+sudo rm -rf ~/.hgrc > /dev/null 2>&1
+sudo rm -rf ~/.hgignore_global > /dev/null 2>&1
+sudo rm -rf ~/.vim > /dev/null 2>&1
+sudo rm -rf ~/.vimrc > /dev/null 2>&1
+# sudo rm -rf ~/.bashrc > /dev/null 2>&1
+# sudo rm -rf ~/.tmux > /dev/null 2>&1
+# sudo rm -rf ~/.tmux.conf > /dev/null 2>&1
+# sudo rm -rf ~/.zsh_prompt > /dev/null 2>&1
+# sudo rm -rf ~/.gitconfig > /dev/null 2>&1
+# sudo rm -rf ~/.antigen > /dev/null 2>&1
+# sudo rm -rf ~/.antigen.zsh > /dev/null 2>&1
+# sudo rm -rf ~/.tigrc > /dev/null 2>&1
+# sudo rm -rf ~/.config > /dev/null 2>&1
+
+# Create symlinks in the home folder
+ln -sf $DOTFILES/zsh/zshrc ~/.zshrc
+ln -sf $DOTFILES/git/gitconfig ~/.gitconfig
+ln -sf $DOTFILES/git/gitignore_global ~/.gitignore_global
+ln -sf $DOTFILES/hg/hgrc ~/.hgrc
+ln -sf $DOTFILES/hg/hgignore_global ~/.hgignore_global
+ln -sf $DOTFILES/vim/vim ~/.vim
+ln -sf $DOTFILES/vim/vimrc ~/.vimrc
+# ln -sf $DOTFILES/bashrc ~/.bashrc
+# ln -sf $DOTFILES/linux-tmux ~/.tmux
+# ln -sf $DOTFILES/zsh/zsh_prompt ~/.zsh_prompt
+# ln -sf $DOTFILES/zsh/zshrc ~/.zshrc
+# ln -sf $DOTFILES/config ~/.config
