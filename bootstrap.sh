72ce37a9d17c1620bd7cc6a409ff184eac79b17b
Dmitry Sichkar <dmmeteo@gmail.com>
2018-05-23 16:50:15 +0300

    tmux

--- bootstrap.sh
+++ bootstrap.sh
@@ -9,9 +9,9 @@ sudo rm -rf ~/.hgrc > /dev/null 2>&1
 sudo rm -rf ~/.hgignore_global > /dev/null 2>&1
 sudo rm -rf ~/.vim > /dev/null 2>&1
 sudo rm -rf ~/.vimrc > /dev/null 2>&1
+sudo rm -rf ~/.tmux.conf > /dev/null 2>&1
+sudo rm -rf ~/.tmux.conf.local > /dev/null 2>&1
 # sudo rm -rf ~/.bashrc > /dev/null 2>&1
-# sudo rm -rf ~/.tmux > /dev/null 2>&1
-# sudo rm -rf ~/.tmux.conf > /dev/null 2>&1
 # sudo rm -rf ~/.zsh_prompt > /dev/null 2>&1
 # sudo rm -rf ~/.gitconfig > /dev/null 2>&1
 # sudo rm -rf ~/.antigen > /dev/null 2>&1
@@ -27,6 +27,8 @@ ln -sf $DOTFILES/hg/hgrc ~/.hgrc
 ln -sf $DOTFILES/hg/hgignore_global ~/.hgignore_global
 ln -sf $DOTFILES/vim/vim ~/.vim
 ln -sf $DOTFILES/vim/vimrc ~/.vimrc
+ln -sf $DOTFILES/tmux/tmux.conf ~/.tmux.conf
+ln -sf $DOTFILES/tmux/tmux.conf.local ~/.tmux.conf.local
 # ln -sf $DOTFILES/bashrc ~/.bashrc
 # ln -sf $DOTFILES/linux-tmux ~/.tmux
 # ln -sf $DOTFILES/zsh/zsh_prompt ~/.zsh_prompt
