d4a3e084df340ebe4b8765bbc226c3dfd29213ad
Dmitry Sichkar <dmmeteo@gmail.com>
2018-06-01 17:00:23 +0300

    remove linuxbrew

--- install/packages.sh
+++ install/packages.sh
@@ -10,39 +10,30 @@ else
             exit
             ;;
         linux*)
-            sh -c "$(curl -fsSL https://raw.githubusercontent.com/Linuxbrew/install/master/install.sh)"
-            test -d ~/.linuxbrew && PATH="$HOME/.linuxbrew/bin:$HOME/.linuxbrew/sbin:$PATH"
-            test -d /home/linuxbrew/.linuxbrew && PATH="/home/linuxbrew/.linuxbrew/bin:/home/linuxbrew/.linuxbrew/sbin:$PATH"
-            test -r ~/.bash_profile && echo "export PATH='$(brew --prefix)/bin:$(brew --prefix)/sbin'":'"$PATH"' >>~/.bash_profile
-            test -r ~/.profile && echo "export PATH='$(brew --prefix)/bin:$(brew --prefix)/sbin'":"\"$PATH\"" >>~/.profile
-            test -r ~/.bashrc && echo "export PATH='$(brew --prefix)/bin:$(brew --prefix)/sbin'":'"$PATH"' >>~/.bashrc
+            alias installer="sudo apt install -y"
+            installer python-pip
             ;;
         darwin*)
             ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
-            test -r ~/.bashrc && echo "export PATH='/usr/local/bin:$PATH'\n" >> ~/.bashrc
+            brew doctor
+            brew update
+            alias installer="brew install"
             ;;
     esac
 fi
 
-brew doctor
-brew update
-
-# So we use all of the packages we are about to install
-source ~/.bashrc
-
-# TODO check packages if each is not already installed
-# coming soon...
-
 # Install packages via Brew
-brew install \
-            zsh \
-            git \
-            vim \
-            figlet \
-            tmux \
-            node \
-            yarn \
-            todolist
+installer \
+        zsh \
+        vim \
+        git \
+        curl \
+        tree \
+        htop \
+        tmux \
+        node \
+        figlet \
+        yarn
 
 # Install packages via pip
 sudo pip install \
