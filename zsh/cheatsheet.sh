f60f54dfcfffa28c55e86fac9fd4fed8d7e54236
Dmitry Sichkar <dmmeteo@gmail.com>
2019-02-06 13:54:18 +0200

    :sparkles: Add cheatsheet
    Add a cheatsheet function to my dotfiles! #1

---
+++ zsh/cheatsheet.sh
@@ -0,0 +1,9 @@
+# Cheatsheets tool
+# TODO: make yurself library and tests
+# https://habr.com/ru/company/ruvds/blog/327248/
+
+function cheat() {
+        echo "vim:\thttps://vim.rtorr.com/"
+        echo "tmux:\thttps://github.com/gpakosz/.tmux"
+        echo "todo:\thttp://todolist.site/"
+}
